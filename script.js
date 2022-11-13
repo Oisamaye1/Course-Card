727854b77fffff2dd0ba1c416c3d7a610f827ad4
Ovioisa Oisamaye Benjamin <99197115+Oisamaye1@users.noreply.github.com>
2022-11-13 19:41:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 let welcomeEl = document.getElementById('welcome-el')
+let cName = document.querySelector('.c-name')
+let cHeading = document.querySelector('.heading')
+let cTitle = document.querySelector('.c-title')
+let cCompletion = document.querySelector('.c-completion')
+
 userName = 'Benjamin'
 welcomeEl.innerText = userName + '🤞'
 
+let courseInfo = {
+    courseName: 'The Frontend Developer Career Path',
+    courseHeading: 'Module 2: HTML & CSS basics',
+    courseTitle: 'Creating heading',
+    courseCompletion: 47
+}
+
+function course(){
+    cName.textContent = courseInfo.courseName
+    cHeading.textContent = courseInfo.courseHeading
+    cTitle.textContent = courseInfo.courseTitle
+    cCompletion.textContent = courseInfo.courseCompletion + '%  of course completed'
+}
+
+course()
